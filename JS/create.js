247f7b0f7c5c537f94f61987999f6fb65a6c2f25
Ivann-Elo <138256764+Ivann-Elo@users.noreply.github.com>
2023-12-05 11:45:21 +0100

    Gestion de saisi

--- JS/create.js
+++ JS/create.js
@@ -1,30 +1,51 @@
 document.getElementById('form').addEventListener('submit',function(event) {
     
-event.preventDefault();
+ event.preventDefault();
 
-const id = document.getElementById('id').value;
-const nom = document.getElementById('nom').value;
-const prenom = document.getElementById('prenom').value;
-const email = document.getElementById('email').value;
 
-const users = {
-    id: id,
-    nom: nom,
-    prenom: prenom,
-    email: email,
-};
+    const id = document.getElementById('id').value;
+    const nom = document.getElementById('nom').value;
+    const prenom = document.getElementById('prenom').value;
+    const email = document.getElementById('email').value;
+    const zoneErreur = document.getElementById('zoneErreur');
 
-fetch('http://fbrc.esy.es/DWWM22053/Api/api.php/users', {
-    method: 'POST', // GET. DELETE. PUT.. 
-    /**headers: {
-        'contentType':'application/JSON',
-    },**/
+    let regExp =  /\d/ ;
+    let regExpMail =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let msg = "Certains champs sont mal saisi !";
+
+    if(regExp.test(nom) || (regExp.test(prenom))){ 
+        console.log("nom ou prenom faux");
+        zoneErreur.innerText = msg;
+    };
+
+    if(!nom || !prenom | !email){
+        console.log("manque valeurs");
+        zoneErreur.innerText =  msg;
+    };
     
-    body: JSON.stringify(users),
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Erreur :', error));
+    if(regExpMail.test(!email)){
+        console.log("mauvais format email");
+        zoneErreur.innerText = msg;
 
-}
-)
+    };
+
+    const users = {
+        id: id,
+        nom: nom,
+        prenom: prenom,
+        email: email,
+    };
+
+    fetch('http://fbrc.esy.es/DWWM22053/Api/api.php/users', {
+        method: 'POST', // GET. DELETE. PUT.. 
+        /**headers: {
+            'contentType':'application/JSON',
+        },**/
+        
+        body: JSON.stringify(users),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Erreur :', error));
+
+});
