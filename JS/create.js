be1fdd8bbc4a9818ec35ce84a62e91340cc7367c
Ivann-Elo <138256764+Ivann-Elo@users.noreply.github.com>
2023-12-05 18:08:15 +0100

    ajout controle

--- JS/create.js
+++ JS/create.js
@@ -1,24 +1,34 @@
 document.getElementById('form').addEventListener('submit',function(event) {
     
+    //event.preventDefault();
+    
+    
+    const id = document.getElementById('id').value;
+    const nom = document.getElementById('nom').value;
+    const prenom = document.getElementById('prenom').value;
+    const email = document.getElementById('email').value;
+    const zoneErreurNom = document.getElementById('zoneErreurNom');
+    const zoneErreurPrenom = document.getElementById('zoneErreurPrenom');
+    const zoneErreurId = document.getElementById('zoneErreurId');
+    
+     let regExp =  /^[a-zA-Z]+$/ ;
+     let regExp2 =  /^\d+$/ ;
+            
+     
+    if(!regExp2.test(id)) {
+            zoneErreurId.innerText = "L'Id est uniquement composé de numéros !";
+            event.preventDefault();
+        };
+    if (!regExp.test(nom)) {
+        zoneErreurNom.innerText = "Le nom ne doit pas comporter de numéros !";
     event.preventDefault();
-   
-   
-       const id = document.getElementById('id').value;
-       const nom = document.getElementById('nom').value;
-       const prenom = document.getElementById('prenom').value;
-       const email = document.getElementById('email').value;
-       const zoneErreur = document.getElementById('zoneErreur');
-   
-       let regExp =  /\d/ ;
-   
-       if(regExp.test(nom) || (regExp.test(prenom))){ 
-           zoneErreur.innerText = "Ne doit pas comporter de numéros !";
-       };
-   
-       // if(!nom || !prenom){
-       //     zoneErreur.innerText =  msg;
-       // };
-       
+        };
+    if (!regExp.test(prenom)) {
+            zoneErreurPrenom.innerText = "Le prénom ne doit pas comporter de numéros !";
+            event.preventDefault();
+        };
+      
+      
        const users = {
            id: id,
            nom: nom,
