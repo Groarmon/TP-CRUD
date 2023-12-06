c02c298518af3c3eb6f13ef8cebcb0c955776d23
Ivann-Elo <138256764+Ivann-Elo@users.noreply.github.com>
2023-12-06 14:30:04 +0100

    Commentaire-read.js

--- JS/read.js
+++ JS/read.js
@@ -1,61 +1,59 @@
+    // Récuperer les élèments HTML
+    let liste = document.getElementById('read');
 
-let liste = document.getElementById('read');
-
-// Récupération JSON de l'API
-fetch('http://fbrc.esy.es/DWWM22053/Api/api.php/users')
-    .then((reponse) => {
-        return reponse.json();
-    })
-    .then((donneeUser) => {
-
-        let valId =  donneeUser.users.records;
-
-        const valNom = donneeUser.users.records.map(index => index[1]);
-        const valPrenom = donneeUser.users.records.map(index => index[2]);
-        const valMail = donneeUser.users.records.map(index => index[3]);
-   
-        var nbrUser = donneeUser.users.records.length;
-        console.log(valId)
-
-        let indice = 0;
-        for (let i = 0; i < nbrUser; i++) {
-            creerListe(valId[indice][0], valNom[i], valPrenom[i], valMail[i]);
-            var li = document.createElement("li");
-            li.classList.add("list-group-item");
-            indice ++;
-        }       
-    });
+    // Récupération les données de l'api avec la méthode Fetch
+    fetch('http://fbrc.esy.es/DWWM22053/Api/api.php/users') 
+        .then((reponse) => {
+            return reponse.json(); // Retourne les éléments de la basse de donnée en Objet
+        })
+        .then((donneeUser) => {
+            let valId =  donneeUser.users.records;
+console.log(donneeUser.users.records);
+            const valNom = donneeUser.users.records.map(index => index[1]);
+            const valPrenom = donneeUser.users.records.map(index => index[2]);
+            const valMail = donneeUser.users.records.map(index => index[3]);
     
-    function creerListe(id, nom, prenom, mail) {
-        // Création de l'élément ul avec la classe bootstrap
-        var ul = document.createElement("ul");
-        ul.classList.add("list-group", "list-group-horizontal", "col-8");
+            var nbrUser = donneeUser.users.records.length;
 
-        // Création des éléments li avec les classes Bootstrap
-        for (let i = 0; i < 3; i++) {
-            var li = document.createElement("li");
-            li.classList.add("list-group-item");
-            var tli = document.createElement("li");
-            tli.classList.add("list-group-item", "text-item", "text-white", "bg-black");
+            let indice = 0;
+            for (let i = 0; i < nbrUser; i++) {
+                creerListe(valId[indice][0], valNom[i], valPrenom[i], valMail[i]);
+                var li = document.createElement("li");
+                li.classList.add("list-group-item");
+                indice ++;
+            }       
+        });
+        
+        function creerListe(id, nom, prenom, mail) {
+            // Création de l'élément ul avec la classe bootstrap
+            var ul = document.createElement("ul");
+            ul.classList.add("list-group", "list-group-horizontal", "col-8");
 
-            if (i === 0) {
-                // Ajout de la première li avec l'ID et le texte spécifique
-                tli.innerText = id;
-                ul.appendChild(tli);
-                li.innerText =  nom;
-            } else if (i === 1) {
-                // Ajout de la deuxième li avec le texte spécifique
-                li.innerText =  prenom;
-            } else  {
-                // Ajout de la troisième li avec le texte spécifique
-                li.innerText = mail;
+            // Création des éléments li avec les classes Bootstrap
+            for (let i = 0; i < 3; i++) {
+                var li = document.createElement("li");
+                li.classList.add("list-group-item");
+                var tli = document.createElement("li");
+                tli.classList.add("list-group-item", "text-item", "text-white", "bg-black");
+
+                if (i === 0) {
+                    // Ajout de la première li avec l'ID et le texte spécifique
+                    tli.innerText = id;
+                    ul.appendChild(tli);
+                    li.innerText =  nom;
+                } else if (i === 1) {
+                    // Ajout de la deuxième li avec le texte spécifique
+                    li.innerText =  prenom;
+                } else  {
+                    // Ajout de la troisième li avec le texte spécifique
+                    li.innerText = mail;
+                }
+
+                // Ajout de l'élément li à l'élément ul
+                ul.appendChild(li);
+
             }
 
-            // Ajout de l'élément li à l'élément ul
-            ul.appendChild(li);
-
+            // Ajout de l'élément ul au corps du document
+            liste.appendChild(ul);
         }
-
-        // Ajout de l'élément ul au corps du document
-         liste.appendChild(ul);
-    }
