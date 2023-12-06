4314a675f9f475bcb59a3f2e8eaa8b28aeb6d0a5
Groarmon <groarmon@gmail.com>
2023-12-06 11:34:53 +0100

    push id inexistant recherche

--- JS/recherche.js
+++ JS/recherche.js
@@ -5,7 +5,23 @@ const rechercheEmail = document.getElementById("emailUpdate");
 
 function fetchUser() {
     var userId = document.getElementById("userId").value;
-    fetch('http://fbrc.esy.es/DWWM22053/Api/api.php/users/' + userId)
+   
+ 
+
+  fetch('http://fbrc.esy.es/DWWM22053/Api/api.php/users/')
+  .then(response => response.json())
+  .then(dataUsers => {
+    const longueurRecords = dataUsers.users.records.length;
+    // On recherche dans le tableau l'occurrence avec la donnée saisie
+    let idExiste = false;
+    for (let i = 0; i < longueurRecords; i++) {
+      if (dataUsers.users.records[i][0] == userId) {
+        idExiste = true;
+        break; 
+      }
+    }
+    if (idExiste) {
+      fetch('http://fbrc.esy.es/DWWM22053/Api/api.php/users/' + userId)
       .then(response => response.json())
       .then(data => {
         rechercheID.value = data.id;
@@ -19,4 +35,10 @@ function fetchUser() {
       .catch((error) => {
         console.error('Erreur:', error);
       });
-  }
+    } else {
+      dontExist();
+    }
+  })
+  .catch(error => console.error('Error:', error));
+
+}
