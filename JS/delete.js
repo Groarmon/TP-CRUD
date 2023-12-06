28f786d8d0a4e2a5911237237da2634569de4d0e
fayssal <merzougui.fayssal@gmail.com>
2023-12-06 15:36:49 +0100

    create.js/patch.js/delete.js -AJOUT COMMENTAIRES

--- JS/delete.js
+++ JS/delete.js
@@ -1,55 +1,62 @@
-document.getElementById('popo').addEventListener('click', function(event) {
+document.getElementById('popo').addEventListener('click', function (event) {
+
   event.preventDefault();
 
   const idDelete = document.getElementById('idDelete').value;
 
   fetch('http://fbrc.esy.es/DWWM22053/Api/api.php/users/')
-  .then(response => response.json())
-  .then(dataUsers => {
-    const longueurRecords = dataUsers.users.records.length;
-    // On recherche dans le tableau l'occurrence avec la donnée saisie
-    let idExiste = false;
-    for (let i = 0; i < longueurRecords; i++) {
-      if (dataUsers.users.records[i][0] == idDelete) {
-        idExiste = true;
-        break; 
-      }
-    }
-    if (idExiste) {
-      // Si l'ID existe, demander confirmation avant la suppression
-      Swal.fire({
-        title: "Êtes-vous sûr de vouloir supprimer cet ID ?",
-        text: "Vous ne pourrez pas revenir en arrière!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Oui",
-        cancelButtonText: "Non"
-      }).then((result) => {
-        if (result.isConfirmed) {
-          fetch("http://fbrc.esy.es/DWWM22053/Api/api.php/users/" + idDelete, {
-            method: "DELETE",
-          })
-          .then(response => response.json())
-          .then(data => {
-            Swal.fire(
-              "Supprimé!",
-              "L'ID à été supprimé",
-              "success"
-            )
+    .then(response => response.json())
+    .then(dataUsers => {
+      const longueurRecords = dataUsers.users.records.length;
 
-          })
-          .catch((error) => {
-            console.error('Erreur:', error);
-          });
-        } else {
-          suppressionAnnule();
+      // On recherche dans le tableau l'occurrence avec la donnée saisie
+      let idExiste = false;
+      for (let i = 0; i < longueurRecords; i++) {
+        if (dataUsers.users.records[i][0] == idDelete) {
+          idExiste = true;
+          break;
         }
-      })
-    } else {
-      dontExist();
-    }
-  })
-  .catch(error => console.error('Error:', error));
+      }
+
+      if (idExiste) {
+        // Si l'ID existe, demander confirmation avant la suppression
+        Swal.fire({
+          title: "Êtes-vous sûr de vouloir supprimer cet ID ?",
+          text: "Vous ne pourrez pas revenir en arrière!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Oui",
+          cancelButtonText: "Non"
+        })
+
+          .then((result) => {
+            if (result.isConfirmed) {
+              fetch("http://fbrc.esy.es/DWWM22053/Api/api.php/users/" + idDelete, {
+                method: "DELETE",
+              })
+                .then(response => response.json())
+                .then(data => {
+                  Swal.fire(
+                    "Supprimé!",
+                    "L'ID à été supprimé",
+                    "success"
+                  )
+                })
+
+                .catch((error) => {
+                  console.error('Erreur:', error);
+                });
+            }
+            else {
+              suppressionAnnule();
+            }
+          })
+      }
+      else {
+        dontExist();
+      }
+    })
+    .catch(error => console.error('Error:', error));
 });
--- JS/patch.js
+++ JS/patch.js
@@ -1,6 +1,11 @@
+// ON ABONNE LE BOUTON SUBMIT DANS LE FORMULAIRE 
+
 document.getElementById('formUpdate').addEventListener('submit', function(event) {
+
+//ON BLOQUE PAR DEFAUT LA SOUMISSION DU FORMULAIRE
     event.preventDefault();
 
+// ON RECUPERE LES INPUTS 
     const idUpdate = document.getElementById('idUpdate').value;
     const nomUpdate = document.getElementById('nomUpdate').value;
     const prenomUpdate = document.getElementById('prenomUpdate').value;
@@ -8,11 +13,14 @@ document.getElementById('formUpdate').addEventListener('submit', function(event)
     const zoneErreurNomUpdate = document.getElementById('zoneErreurNomUpdate');
     const zoneErreurPrenomUpdate = document.getElementById('zoneErreurPrenomUpdate');
 
+// ON DEFINIT LES EXPRESSIONS REGULIERES AUTORISEE
     let regExp =  /^[a-zA-Z]+$/ ;
-    
+
+// ON INITIALISE LES BALISES A VIDE 
     zoneErreurPrenomUpdate.innerText = "";
     zoneErreurNomUpdate.innerText = "";
 
+// ON INIT L'OBJET userUpdates AVEC LES VALEURS RECUPEREES DANS LE FORMULAIRE
     const usersUpdate = {
 
         nom: nomUpdate,
@@ -20,11 +28,16 @@ document.getElementById('formUpdate').addEventListener('submit', function(event)
         email: emailUpdate,
     };
 
-
+// ON RECUPERE LA BASE DE DONNEE 
         fetch('http://fbrc.esy.es/DWWM22053/Api/api.php/users/')
+
+// PROMESSE DE REPONSE ENVOYEE ET RENVOIE UN OBJET AU FORMAT JSON
         .then(response => response.json())
+
+// ON FAIT UNE FONCTION FLECHER <3
         .then(dataUsers => {
           const longueurRecords = dataUsers.users.records.length;
+
           // On recherche dans le tableau l'occurrence avec la donnée saisie
           let emailExiste = false;
           for (let i = 0; i < longueurRecords; i++) {
@@ -33,16 +46,17 @@ document.getElementById('formUpdate').addEventListener('submit', function(event)
                 break; 
               }
             }
-          
-          if(emailExiste ){
-      
+
+    // ON FAIT UN POP UP SI EMAIL EXISTE
+          if(emailExiste){
               swal.fire({
                   title: "Le mail existe déjà.",
                   icon: "info"
               })
-      
-          } else {
-
+           } 
+    // ON CONTROLE LES REGEXPS
+    // let regExp =  /^[a-zA-Z]+$/ ; SEULEMENT DES STRINGS AUTORISEE
+          else {
             if (!regExp.test(nomUpdate)){
                 zoneErreurNomUpdate.innerText = "Le nom ne doit pas comporter de numéros !";
                     event.preventDefault();
@@ -51,6 +65,7 @@ document.getElementById('formUpdate').addEventListener('submit', function(event)
                     zoneErreurPrenomUpdate.innerText = "Le prénom ne doit pas comporter de numéros !";
                     event.preventDefault();
                 }
+    // NOUS Y SOMMES: ON ENVOIE LA DANS LA BDD (pour Jean-Eudes) LES MODIFICATIONS.
                  else {
           
                     fetch("http://fbrc.esy.es/DWWM22053/Api/api.php/users/" + idUpdate, {
@@ -71,7 +86,3 @@ document.getElementById('formUpdate').addEventListener('submit', function(event)
         .catch(error => console.error('Error:', error));
       });
       
-
-      
-    //console.log(donneeUser.users.records[1][0] === idUpdate);
-    
